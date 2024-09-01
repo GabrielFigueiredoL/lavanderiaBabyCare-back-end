4c27553f628dbc66dc06899c80262788521af81c
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-09-01 04:59:52 -0300

    tada: first commit

---
+++ src/main/java/com/gabrielfigueiredol/lavanderiababycare/LavanderiababycareApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielfigueiredol.lavanderiababycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavanderiababycareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavanderiababycareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielfigueiredol/lavanderiababycare/LavanderiababycareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfigueiredol.lavanderiababycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanderiababycareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
