6bc8d5822b8687df9a743502a45e3aea5e4c7f1e
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-09-03 11:27:36 -0300

    feat: add order service

---
+++ src/main/java/com/gabrielfigueiredol/lavanderiababycare/services/OrderService.java
@@ -0,0 +1,67 @@
+package com.gabrielfigueiredol.lavanderiababycare.services;
+
+import com.gabrielfigueiredol.lavanderiababycare.entities.Order;
+import com.gabrielfigueiredol.lavanderiababycare.exceptions.OrderNotFoundException;
+import com.gabrielfigueiredol.lavanderiababycare.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderStatusService orderStatus;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public List<Order> findByStatusId(Integer id) {
+        return orderRepository.findByStatusId(id);
+    }
+
+    public Order insert(Order order) {
+        String uuid = UUID.randomUUID().toString();
+        Timestamp updated_at = new Timestamp(System.currentTimeMillis());
+        order.setId(uuid);
+        order.setStatus(orderStatus.getOrderStatusById(order.getOrderStatusId()));
+        return orderRepository.save(order);
+    }
+
+    public void delete(String id) {
+        if (!orderRepository.existsById(id)) {
+            throw new OrderNotFoundException();
+        }
+        orderRepository.deleteById(id);
+    }
+
+    public Order update(String id, Order order) {
+        if (!orderRepository.existsById(id)) {
+            throw new OrderNotFoundException();
+        }
+        Order orderReference = orderRepository.getReferenceById(id);
+        updateData(orderReference, order);
+        return orderRepository.save(orderReference);
+    }
+
+    public void updateData(Order orderReference, Order order) {
+        orderReference.setClientName(order.getClientName());
+        orderReference.setClientPhone(order.getClientPhone());
+        orderReference.setPickupDate(order.getPickupDate());
+        orderReference.setDeliveryDate(order.getDeliveryDate());
+        orderReference.setCep(orderReference.getCep());
+        orderReference.setAddress(orderReference.getAddress());
+        orderReference.setDistrict(orderReference.getDistrict());
+        orderReference.setNumber(orderReference.getNumber());
+        orderReference.setComplement(orderReference.getComplement());
+        orderReference.setShipping(orderReference.getShipping());
+        orderReference.setDiscount(orderReference.getDiscount());
+        orderReference.setStatus(orderStatus.getOrderStatusById(order.getOrderStatusId()));
+    }
+}
