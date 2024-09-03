c6bdb7c2d2e62eb62d853290b172a7405882b3a4
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-09-03 11:27:01 -0300

    feat: refactor order entity to use proper column naming and relationships

--- src/main/java/com/gabrielfigueiredol/lavanderiababycare/entities/Order.java
+++ src/main/java/com/gabrielfigueiredol/lavanderiababycare/entities/Order.java
@@ -7,18 +7,23 @@ import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_order")
+@Table(name = "orders")
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-    private String client_name;
-    private String client_phone;
 
-    private Timestamp pickup_date;
-    private Timestamp delivery_date;
+    @Column(name = "client_name")
+    private String clientName;
+    @Column(name = "client_phone")
+    private String clientPhone;
+
+    @Column(name = "pickup_date")
+    private Timestamp pickupDate;
+    @Column(name = "delivery_date")
+    private Timestamp deliveryDate;
 
     private String cep;
     private String address;
@@ -29,24 +34,28 @@ public class Order implements Serializable {
     private Integer shipping;
     private Integer discount;
 
-    private String status;
+    @Transient
+    private Integer orderStatusId;
+    @ManyToOne
+    @JoinColumn(name = "status")
+    private OrderStatus status;
 
     public Order(){}
 
-    public Order(String id, String client_name, String client_phone, Timestamp pickup_date, Timestamp delivery_date, String cep, String address, String district, String number, String complement, Integer shipping, String status, Integer discount) {
+    public Order(String id, String clientName, String clientPhone, Timestamp pickupDate, Timestamp deliveryDate, String cep, String address, String district, String number, String complement, Integer shipping, Integer discount, Integer orderStatusId) {
         this.id = id;
-        this.client_name = client_name;
-        this.client_phone = client_phone;
-        this.pickup_date = pickup_date;
-        this.delivery_date = delivery_date;
+        this.clientName = clientName;
+        this.clientPhone = clientPhone;
+        this.pickupDate = pickupDate;
+        this.deliveryDate = deliveryDate;
         this.cep = cep;
         this.address = address;
         this.district = district;
         this.number = number;
         this.complement = complement;
         this.shipping = shipping;
-        this.status = status;
         this.discount = discount;
+        this.orderStatusId = orderStatusId;
     }
 
     public String getId() {
@@ -57,60 +66,36 @@ public class Order implements Serializable {
         this.id = id;
     }
 
-    public String getStatus() {
-        return status;
+    public String getClientName() {
+        return clientName;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
     }
 
-    public Integer getDiscount() {
-        return discount;
+    public Timestamp getPickupDate() {
+        return pickupDate;
     }
 
-    public void setDiscount(Integer discount) {
-        this.discount = discount;
+    public void setPickupDate(Timestamp pickupDate) {
+        this.pickupDate = pickupDate;
     }
 
-    public Integer getShipping() {
-        return shipping;
+    public String getClientPhone() {
+        return clientPhone;
     }
 
-    public void setShipping(Integer shipping) {
-        this.shipping = shipping;
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
     }
 
-    public String getComplement() {
-        return complement;
+    public Timestamp getDeliveryDate() {
+        return deliveryDate;
     }
 
-    public void setComplement(String complement) {
-        this.complement = complement;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
+    public void setDeliveryDate(Timestamp deliveryDate) {
+        this.deliveryDate = deliveryDate;
     }
 
     public String getCep() {
@@ -121,36 +106,68 @@ public class Order implements Serializable {
         this.cep = cep;
     }
 
-    public Timestamp getDelivery_date() {
-        return delivery_date;
+    public String getAddress() {
+        return address;
     }
 
-    public void setDelivery_date(Timestamp delivery_date) {
-        this.delivery_date = delivery_date;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
-    public Timestamp getPickup_date() {
-        return pickup_date;
+    public String getDistrict() {
+        return district;
     }
 
-    public void setPickup_date(Timestamp pickup_date) {
-        this.pickup_date = pickup_date;
+    public void setDistrict(String district) {
+        this.district = district;
     }
 
-    public String getClient_phone() {
-        return client_phone;
+    public String getNumber() {
+        return number;
     }
 
-    public void setClient_phone(String client_phone) {
-        this.client_phone = client_phone;
+    public void setNumber(String number) {
+        this.number = number;
     }
 
-    public String getClient_name() {
-        return client_name;
+    public String getComplement() {
+        return complement;
     }
 
-    public void setClient_name(String client_name) {
-        this.client_name = client_name;
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public Integer getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(Integer shipping) {
+        this.shipping = shipping;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    public Integer getOrderStatusId() {
+        return orderStatusId;
+    }
+
+    public void setOrderStatusId(Integer orderStatusId) {
+        this.orderStatusId = orderStatusId;
+    }
+    
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
     }
 
     @Override
