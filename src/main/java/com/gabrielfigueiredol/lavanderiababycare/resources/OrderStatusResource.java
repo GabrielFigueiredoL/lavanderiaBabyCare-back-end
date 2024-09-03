1ac8ec037424b51ad8b069a8dfb2783f34bc0d7f
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-09-03 11:23:22 -0300

    feat: add order status resource

---
+++ src/main/java/com/gabrielfigueiredol/lavanderiababycare/resources/OrderStatusResource.java
@@ -0,0 +1,22 @@
+package com.gabrielfigueiredol.lavanderiababycare.resources;
+
+import com.gabrielfigueiredol.lavanderiababycare.entities.OrderStatus;
+import com.gabrielfigueiredol.lavanderiababycare.services.OrderStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/order-status")
+public class OrderStatusResource {
+    @Autowired
+    private OrderStatusService orderStatus;
+
+    @GetMapping
+    public ResponseEntity<List<OrderStatus>> findAll() {
+        List<OrderStatus> orderStatusList = orderStatus.findAll();
+        return ResponseEntity.ok().body(orderStatusList);
+    }
+}
