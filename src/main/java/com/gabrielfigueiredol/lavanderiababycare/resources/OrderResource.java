c3c4ec8017f3c13d0145369c1630af6573b711a2
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-09-03 11:27:47 -0300

    feat: add order resource

---
+++ src/main/java/com/gabrielfigueiredol/lavanderiababycare/resources/OrderResource.java
@@ -0,0 +1,50 @@
+package com.gabrielfigueiredol.lavanderiababycare.resources;
+
+import com.gabrielfigueiredol.lavanderiababycare.entities.Order;
+import com.gabrielfigueiredol.lavanderiababycare.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+    @Autowired
+    private OrderService orderService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> orderList = orderService.findAll();
+        return ResponseEntity.ok().body(orderList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<List<Order>> findByStatusId(@PathVariable Integer id) {
+        List<Order> filteredOrderList = orderService.findByStatusId(id);
+        return ResponseEntity.ok().body(filteredOrderList);
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> insert(@RequestBody Order order) {
+        order = orderService.insert(order);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(order.getId()).toUri();
+        return ResponseEntity.created(uri).body(order);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        orderService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Order> update(@PathVariable String id, @RequestBody Order order){
+        order = orderService.update(id, order);
+        return ResponseEntity.ok().body(order);
+    }
+}
