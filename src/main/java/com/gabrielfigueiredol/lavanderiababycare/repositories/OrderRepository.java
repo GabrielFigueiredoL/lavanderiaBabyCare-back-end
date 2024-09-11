342483afd5bc3116817adf226371dab3d088ef41
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-09-11 19:41:57 -0300

    feat: implement find daily orders query

--- src/main/java/com/gabrielfigueiredol/lavanderiababycare/repositories/OrderRepository.java
+++ src/main/java/com/gabrielfigueiredol/lavanderiababycare/repositories/OrderRepository.java
@@ -2,9 +2,15 @@ package com.gabrielfigueiredol.lavanderiababycare.repositories;
 
 import com.gabrielfigueiredol.lavanderiababycare.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
     List<Order> findByStatusId(Integer id);
+
+    @Query("SELECT o FROM Order o WHERE o.pickupDate = :currentDate OR o.deliveryDate = :currentDate")
+    List<Order> findDailyOrders(@Param("currentDate") LocalDate currentDate);
 }
